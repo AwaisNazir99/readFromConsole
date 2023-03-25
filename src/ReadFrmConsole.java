f29aac76be48f40ebf3a067b83d66c31453ca425
AwaisNazir99 <awais.nazir99@gmail.com>
2023-03-25 21:51:01 +0500

    Initial commit

---
+++ src/ReadFrmConsole.java
@@ -0,0 +1,7 @@
+public class ReadFrmConsole {
+    public static void main(String[] args) {
+            String name = System.console().readLine("Write your name= ");
+            System.out.println(name);
+
+    }
+}
